cc129aa4c233d30d95e439a0f66ac44034c64ee2
R. A. Souza <alves.wm@gmail.com>
2016-05-12 21:02:19 -0300

    put $data in array

--- resources/views/relatorios/pdf.blade.php
+++ resources/views/relatorios/pdf.blade.php
@@ -15,19 +15,19 @@
         <h2>Dados do Cliente</h2>
 
         <p><b>Cliente:</b> {{ $os->agenda->cliente->nome }} <b>Data:</b> {{ \Carbon\Carbon::now()->format('d/m/Y') }}</p>
-        <p><b>Hora da Liberação:</b> {{ $liberacao }} <b>Hora do Início do Inventário: </b> {{ $inicio }} </p>
-        <p> <b>Hora do Termino do Inventário: </b> {{ $termino }} <b>Hora Final:</b> {{ $final }} </p>
-        <p><b>Período:</b> {{ $periodo }} <b>Tempo de Duração do Inventário:</b> {{ $duracao }}</p>
+        <p><b>Hora da Liberação:</b> {{ $data['liberacao'] }} <b>Hora do Início do Inventário: </b> {{ $data['inicio }} </p>
+        <p> <b>Hora do Termino do Inventário: </b> {{ $data['termino'] }} <b>Hora Final:</b> {{ $data['final'] }} </p>
+        <p><b>Período:</b> {{ $periodo }} <b>Tempo de Duração do Inventário:</b> {{ $data['duracao'] }}</p>
         <p><b>Responsável da AVM:</b> {{ $os->coordenador->nome }} <b>Responsável do Cliente:</b> {{ ucfirst($os->agenda->cliente->ficha->gerente) }} </p>
 
         <h2>Estatísticas do Inventário</h2>
 
         <p><b>Qtde de Peças Inventariadas:</b> {{ $os->pecas() }}</p>
-        <p><b>Qtde de Peças Auditadas:</b> {{ $auditadas }} ({{ round($auditadas/$os->pecas()) }})%.</p>
-        <p><b>Qtde de Peças s/ Etiquetas (Manuais):</b> {{ $etiqueta }} ({{ round($etiqueta/$os->pecas()) }})%.</p>
-        <p><b>Qtde de Peças de Fora do Inventario:</b> {{ $fora }} <b>Entregue para:</b> {{$entregue}}</p>
+        <p><b>Qtde de Peças Auditadas:</b> {{ $data['auditadas'] }} ({{ round($data['auditadas']/$os->pecas()) }})%.</p>
+        <p><b>Qtde de Peças s/ Etiquetas (Manuais):</b> {{ $data['etiqueta'] }} ({{ round($data['etiqueta']/$os->pecas()) }})%.</p>
+        <p><b>Qtde de Peças de Fora do Inventario:</b> {{ $data['fora'] }} <b>Entregue para:</b> {{$data['entregue']}}</p>
 
-        <p><b>Número de Inventariantes:</b> {{ count($os->inventariantes) }} <b>Tempo Ocioso:</b> {{ $ocioso }}</p>
+        <p><b>Número de Inventariantes:</b> {{ count($os->inventariantes) }} <b>Tempo Ocioso:</b> {{ $data['ocioso'] }}</p>
 
         <ul>
             @foreach($inventariantes as $inventariante)
@@ -41,7 +41,7 @@
             <p>{{$ambiente->nome}} <b>Início:</b> {{ $ambiente->inicio }} <b>Fim:</b> {{ $ambiente->fim }}</p>
         @endforeach
 
-        <p>{{ $lugares }}</p>
+        <p>{{ $data['lugares'] }}</p>
 
         <h2>De acordo</h2>
         <p><b>A Criação dos setores foi acompanhada:</b> </p>
