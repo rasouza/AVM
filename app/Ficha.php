c1363429609be2bdbd5827283a893ece0e4aa928
R. A. Souza <alves.wm@gmail.com>
2016-08-17 01:11:36 -0300

    fixes #8 bug do relatorio em pdf

--- app/Ficha.php
+++ app/Ficha.php
@@ -8,6 +8,9 @@ class Ficha extends Model
 {
     protected $guarded = [];
 
+    public function getGerenteAttribute($v) { return ucfirst($v); }
+    public function setGerenteAttribute($v) { $this->attributes['gerente'] = ucfirst($v); }
+
     public function cliente() { return $this->belongsTo('App\Cliente'); }
     public function uf() { return $this->belongsTo('App\Uf'); }
 }
